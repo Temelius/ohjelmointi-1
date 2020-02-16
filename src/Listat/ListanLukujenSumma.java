0c0065b8e75858fc0b744757274d02784c2126ed
Temelius <teme989@gmail.com>
2020-02-16 05:29:56 +0200

    Add task ListanLukujenSumma

---
+++ src/Listat/ListanLukujenSumma.java
@@ -0,0 +1,49 @@
+package Listat;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Ohjelmaan on toteutettu valmiina pohja, joka lukee käyttäjältä lukuja
+ * listalle. Syötteiden lukeminen päätetään kun käyttäjä syöttää luvun -1.
+ * 
+ * Lisää ohjelmaan toiminnallisuus, joka lukujen lukemisen jälkeen laskee ja
+ * tulostaa listalla olevien lukujen summan.
+ * 
+ * Tämän tehtävän on laatinut Helsingin yliopiston Agile Education Research
+ * -tutkimusryhmä. Tehtävä on lisensoitu on lisensoitu Creative Commons BY-NC-SA
+ * 4.0 -lisenssillä ja se on julkaistu kurssilla https://ohjelmointi-19.mooc.fi.
+ */
+public class ListanLukujenSumma {
+
+    public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+        List<Integer> luvut = new ArrayList<Integer>();
+
+        System.out.println("Syötä kokonaislukuja listalle (-1 lopettaa):");
+
+        while (true) {
+            int syote = lukija.nextInt();
+
+            if (syote == -1) {
+                break;
+            } else {
+                luvut.add(syote);
+            }
+        }
+        lukija.close();
+
+        System.out.println(); // Tyhjä rivi tulosteeseen
+
+        // TODO: Täydennä koodia tehtävänannon mukaisesti
+        
+        int summa = 0;
+        
+        for (int i = 0; i < luvut.size(); i++) {
+        	summa += luvut.get(i);
+        }
+        
+        System.out.println("Summa: " + summa);
+    }
+}
