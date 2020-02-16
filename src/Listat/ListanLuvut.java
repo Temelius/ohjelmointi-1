e9675a9c1b0faa4622103dd382c463834946bc66
Temelius <teme989@gmail.com>
2020-02-16 04:29:30 +0200

    Add task ListanLuvut.java

---
+++ src/Listat/ListanLuvut.java
@@ -0,0 +1,45 @@
+package Listat;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Ohjelmaan on toteutettu valmiina pohja, joka lukee käyttäjältä lukuja
+ * listalle. Syötteiden lukeminen päätetään kun käyttäjä syöttää luvun -1.
+ * 
+ * Lisää ohjelmaan toiminnallisuus, joka lukujen lukemisen jälkeen tulostaa
+ * käyttäjän syöttämät luvut. Syötteen lukemisen päättämisestä ilmoittavaa lukua
+ * ei tulosteta.
+ * 
+ * Tämän tehtävän on laatinut Helsingin yliopiston Agile Education Research
+ * -tutkimusryhmä. Tehtävä on lisensoitu on lisensoitu Creative Commons BY-NC-SA
+ * 4.0 -lisenssillä ja se on julkaistu kurssilla https://ohjelmointi-19.mooc.fi.
+ */
+public class ListanLuvut {
+    public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+        List<Integer> luvut = new ArrayList<Integer>();
+
+        System.out.println("Syötä kokonaislukuja listalle (-1 lopettaa):");
+        while (true) {
+            int syote = lukija.nextInt();
+
+            if (syote == -1) {
+                break;
+            } else {
+                luvut.add(syote);
+            }
+        }
+        lukija.close();
+
+        System.out.println(); // Tyhjä rivi tulosteeseen
+
+        // TODO: Täydennä koodia tehtävänannon mukaisesti
+        
+        for (int i = 0; i < luvut.size(); i++) {
+        	System.out.println(luvut.get(i));	
+        }
+        
+    }
+}
