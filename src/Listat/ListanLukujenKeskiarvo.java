d4c53aeffbb7b00100ade29e3b78c546b9744eb7
Temelius <teme989@gmail.com>
2020-02-16 16:46:52 +0200

    Add task information

--- src/Listat/ListanLukujenKeskiarvo.java
+++ src/Listat/ListanLukujenKeskiarvo.java
@@ -4,6 +4,18 @@ import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
 
+/**
+ * Toteuta ohjelma, joka lukee käyttäjältä lukuja listalle. Syötteiden lukeminen
+ * päätetään kun käyttäjä syöttää luvun -1.
+ * 
+ * Kun lukujen lukeminen lopetetaan, laske listalla olevien lukujen keskiarvo ja
+ * tulosta se.
+ * 
+ * Tämän tehtävän on laatinut Helsingin yliopiston Agile Education Research
+ * -tutkimusryhmä. Tehtävä on lisensoitu on lisensoitu Creative Commons BY-NC-SA
+ * 4.0 -lisenssillä ja se on julkaistu kurssilla https://ohjelmointi-19.mooc.fi.
+ */
+
 public class ListanLukujenKeskiarvo {
 
 	public static void main(String[] args) {
