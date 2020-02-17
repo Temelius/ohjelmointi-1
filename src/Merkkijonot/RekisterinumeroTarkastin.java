c0384f4793ccd72912197a34735d8985c949981e
Temelius <teme989@gmail.com>
2020-02-17 03:20:33 +0200

    Add task RekisterinumeroTarkastin

--- src/Merkkijonot/RekisterinumeroTarkastin.java
+++ src/Merkkijonot/RekisterinumeroTarkastin.java
@@ -5,8 +5,23 @@ import java.util.Scanner;
 public class RekisterinumeroTarkastin {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner lukija = new Scanner(System.in);
+		
+		while (true) {
+			System.out.print("Syötä rekisterinumero (q-kirjain lopettaa): ");
+			String reknro = lukija.nextLine();
+			if ("q".equals(reknro)) {
+				break;
+			} else {
+				if (reknro.matches("[A-ZÅÖÄ]{2,3}-[0-9]{1,3}")) {
+					System.out.println(reknro + " on kelvollinen rekisterinumero.");
+				} else {
+					System.out.println(reknro + " ei ole kelvollinen rekisterinumero.");
+				}
+			}
+			System.out.println(); // Tyhjä rivi
+		}
+		lukija.close();
 	}
 
 }
