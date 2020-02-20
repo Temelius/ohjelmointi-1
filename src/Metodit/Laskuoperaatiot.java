17c88cfd868948db3ff8f7568c7c9cf8266f0f4a
Temelius <teme989@gmail.com>
2020-02-20 20:27:46 +0200

    Add task Laskuoperaatiot

---
+++ src/Metodit/Laskuoperaatiot.java
@@ -0,0 +1,20 @@
+package Metodit;
+
+public class Laskuoperaatiot {
+	
+	// Tehtävässä ei tulosteta mitään tarkistusjärjestelmän takia.
+	// LaskuoperaatiotTest.java luokassa on testit mukana.
+
+	public static void main(String[] args) {
+	
+	}
+	
+	public static double summa(double i, double x) {
+		return i + x;
+	}
+	
+	public static double erotus(double i, double x) {
+		return i - x;
+	}
+
+}
