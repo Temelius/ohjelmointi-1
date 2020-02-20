edba6ea214cd99ae729d77a20616ee9ffe6c5486
Temelius <teme989@gmail.com>
2020-02-20 13:50:29 +0200

    Fix MarsinLampotilat pienin / suurin and add keskiarvo

--- src/Taulukot/MarsinLampotilat.java
+++ src/Taulukot/MarsinLampotilat.java
@@ -3,19 +3,12 @@ package Taulukot;
 import java.util.Scanner;
 import java.util.Arrays;
 
-/* Toteuta ohjelma Mars-planeetan lämpötilamittausten tarkasteluun.
- * Marsin alin lämpötila (pinnalla) on -140 astetta, korkein lämpötila on 20 astetta. 
- * Ohjelmalle tulee syöttää 10 hyväksyttyä mittaustulosta, jotka ohjelma kerää int[]-tyyppiseen taulukkoon, 
- * jonka pituus on 10. Jos käyttäjä syöttää luvun, joka on pienempi kuin -140 tai suurempi kuin 20, 
- * lukua ei laiteta taulukkoon vaan se pyydetään uudelleen esimerkkisuorituksen mukaisesti.
- * Lopuksi ohjelman tulee tulostaa lämpötilamittausten keskiarvo sekä minimi- ja maksimiarvot. 
- * Tehtävässä on tarkoitus harjoitella taulukoiden käyttöä, joten listojen käyttäminen on tässä tehtävässä kiellettyä.
- * Huom! Älä pyöristä lopussa tulostettavaa keskiarvoa */
-
 public class MarsinLampotilat {
 	public static void main(String[] args) {
 		Scanner lukija = new Scanner(System.in);
 		int[] lampotilat = new int[10];
+		
+		double lampotilatSum = 0;
 		
 		int i = 0;
 		while (i < 10) {
@@ -25,26 +18,19 @@ public class MarsinLampotilat {
 				System.out.println("Anna lämpötila väliltä -140 - +20!");
 			} else {
 				lampotilat[i] = luku;
+				lampotilatSum += luku;
 				i++;
 			}
 		}
-		System.out.println(Arrays.toString(lampotilat));
-		/*
-		 * TODO Alla oleva ei toimi tällä hetkellä
+		
+		Arrays.sort(lampotilat);
+		double keskiarvo = lampotilatSum / lampotilat.length;
 		int pienin = lampotilat[0];
-		int suurin = lampotilat[0];
+		int suurin = lampotilat[9];
 		
-		for (int x = 0; x < lampotilat.length; i++) {
-			if (pienin > lampotilat[x]) {
-				pienin = lampotilat[x];
-			}
-			if (suurin < lampotilat[x]) {
-				suurin = lampotilat[x];
-			}
-		}
-		
+		System.out.println("Mittausten keskiarvo: " + keskiarvo);
 		System.out.println("Pienin mittaustulos: " + pienin);
-		System.out.println("Suurin mittaustulos: " + suurin); */
+		System.out.println("Suurin mittaustulos: " + suurin);
 		
 		lukija.close();
 	}
